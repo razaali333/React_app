3f352be63cd9037853d980e2cdbcfd01fb154eef
razaali333 <raza.aursoft@gmail.com>
2018-12-27 00:50:56 +0500

    Updates

---
+++ precache-manifest.b979478f5cc46b49b61840cb1351aada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f41efef2db08d9f3a2a",
+    "url": "/React_app/static/js/runtime~main.0f41efef.js"
+  },
+  {
+    "revision": "06a4ddb48bbc1ef0c294",
+    "url": "/React_app/static/js/main.06a4ddb4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/React_app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "06a4ddb48bbc1ef0c294",
+    "url": "/React_app/static/css/main.9da64a6c.chunk.css"
+  },
+  {
+    "revision": "9e80c957450ace00245e96b964480f2f",
+    "url": "/React_app/index.html"
+  }
+];
